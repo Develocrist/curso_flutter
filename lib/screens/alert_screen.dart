5f2df96eb0338e0269138d6501990eacca074429
Cristofer <ctorrejoncortes1996@gmail.com>
2022-09-27 15:00:46 -0300

    estructura de calculadora definida, pero falta

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -39,22 +39,33 @@ class Calculadora extends State<MyCalculator> {
 
   //formula para realizar la operacion aritmetica
   void OperacionMatematica() {
-    if (my_form_key.currentState!.validate()) {
-      double demanda = double.parse(controllerDemanda.text);
-      double orden = double.parse(controllerOrden.text);
-      double costoUnitario = double.parse(controllerCostoUnitario.text);
-      double costoMantencion = double.parse(controllerMantencion.text);
+    double demanda = double.parse(controllerDemanda.text);
+    double orden = double.parse(controllerOrden.text);
+    double costoUnitario = double.parse(controllerCostoUnitario.text);
+    double costoMantencion = double.parse(controllerMantencion.text);
 
-      double cantidadOptima = sqrt((2 * demanda * orden) / costoMantencion);
-      String r = cantidadOptima.toStringAsFixed(0);
-      double numOrdenes = demanda / cantidadOptima;
-      String ordenes = numOrdenes.toStringAsFixed(0);
+    double cantidadOptima = sqrt((2 * demanda * orden) / costoMantencion);
+    String r = cantidadOptima.toStringAsFixed(0);
+    double numOrdenes = demanda / cantidadOptima;
+    String ordenes = numOrdenes.toStringAsFixed(0);
 
-      setState(() {
-        mostrarImc = 'La cantidad optima de pedido es de $r unidades';
-        mostrarNumOrdenes = 'El numero de ordenes esperado es de $ordenes ';
-      });
-    }
+    setState(() {
+      mostrarImc = 'La cantidad optima de pedido es de $r unidades';
+      mostrarNumOrdenes = 'El numero de ordenes esperado es de $ordenes ';
+    });
+  }
+
+  void limpiaCampos() {
+    double demanda = double.parse(controllerDemanda.text);
+    double orden = double.parse(controllerOrden.text);
+    double costoUnitario = double.parse(controllerCostoUnitario.text);
+    double costoMantencion = double.parse(controllerMantencion.text);
+    setState(() {
+      controllerCostoUnitario.clear();
+      controllerOrden.clear();
+      controllerDemanda.clear();
+      controllerMantencion.clear();
+    });
   }
 
 //parte visual
@@ -64,234 +75,155 @@ class Calculadora extends State<MyCalculator> {
     final double _height = logicalSize.height;
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.indigo,
         title: const Text('Calculadora EOQ'),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {},
-        ),
       ),
-      body: Form(
-        key: my_form_key,
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
+      body: ListView(key: my_form_key, children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(" Demanda (D): "),
+            Container(
+              width: 60,
+              height: 50,
+              child: TextFormField(
+                controller: controllerDemanda,
+                validator: (value) {
+                  if (value!.isEmpty) return "Ingresa la demanda";
+                },
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            const Text("Costo Orden (K): "),
+            Container(
+              width: 60,
+              height: 50,
+              child: TextFormField(
+                controller: controllerOrden,
+                validator: (value) {
+                  if (value!.isEmpty) return "Ingresa el costo de orden";
+                },
+                keyboardType: TextInputType.number,
+              ),
+            )
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            const Text("Mantención (H): "),
+            Container(
+              width: 60,
+              height: 50,
+              child: TextFormField(
+                  keyboardType: TextInputType.number,
+                  controller: controllerMantencion,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Ingresar costo de mantencion";
+                    }
+                  }),
+            ),
+            const Text("Costo unitario (i): "),
+            Container(
+              width: 60,
+              height: 50,
+              child: TextFormField(
+                keyboardType: TextInputType.number,
+                controller: controllerCostoUnitario,
+                validator: (value) {
+                  if (value!.isEmpty) return "Ingresa el costo unitario";
+                },
+              ),
+            )
+          ]),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              // Container(
-              //   width: 500,
-              //   height: 130,
-              //   decoration: const BoxDecoration(
-              //       color: Color.fromARGB(255, 118, 132, 202),
-              //       borderRadius: BorderRadius.only(
-              //         bottomLeft: Radius.circular(80),
-              //         bottomRight: Radius.circular(80),
-              //       )),
-              //   // child: Stack(
-              //   //   children: <Widget>[
-              //   //     Align(
-              //   //       alignment: Alignment.center,
-              //   //       child: new Image(
-              //   //         width: 200.0,
-              //   //         height: 150.0,
-              //   //         image: new AssetImage('assets/images/iconagii2.png'),
-              //   //       ),
-              //   //     )
-              //   //   ],
-              //   // ),
-              // ),
-
-              //agregamos otro container
-              Container(
-                padding: const EdgeInsets.only(top: 20),
-                child: Column(children: [
-                  Container(
-                    width: 220,
-                    height: 80,
-                    padding: const EdgeInsets.all(7),
-                    decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(50)),
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(color: Colors.black, blurRadius: 5)
-                        ]),
-                    child: TextFormField(
-                      controller: controllerDemanda,
-                      validator: (value) {
-                        if (value!.isEmpty) return "Ingresa la demanda";
-                      },
-                      decoration: const InputDecoration(
-                          hintText: "Ingresar demanda",
-                          icon: Icon(Icons.account_balance_wallet_outlined,
-                              color: Colors.purpleAccent)),
-                      keyboardType: TextInputType.number,
-                    ),
-                  ),
-                ]),
-              ),
-              const Divider(),
-              Container(
-                padding: const EdgeInsets.only(top: 20),
-                child: Column(children: [
-                  Container(
-                    width: 220,
-                    height: 80,
-                    padding: const EdgeInsets.all(7),
-                    decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(50)),
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(color: Colors.black, blurRadius: 5)
-                        ]),
-                    child: TextFormField(
-                      controller: controllerOrden,
-                      validator: (value) {
-                        if (value!.isEmpty) return "Ingresa el costo de orden";
-                      },
-                      decoration: const InputDecoration(
-                          hintText: "Ingresar costo orden",
-                          icon: Icon(Icons.account_balance_wallet_outlined,
-                              color: Colors.purpleAccent)),
-                      keyboardType: TextInputType.number,
-                    ),
-                  ),
-                ]),
-              ),
-              const Divider(),
-              Container(
-                padding: const EdgeInsets.only(top: 20),
-                child: Column(children: [
-                  Container(
-                    width: 220,
-                    height: 80,
-                    padding: const EdgeInsets.all(7),
-                    decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(50)),
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(color: Colors.black, blurRadius: 5)
-                        ]),
-                    child: TextFormField(
-                      controller: controllerCostoUnitario,
-                      validator: (value) {
-                        if (value!.isEmpty) return "Ingresa el costo unitario";
-                      },
-                      decoration: const InputDecoration(
-                          hintText: "Costo unitario",
-                          icon: Icon(Icons.account_balance_wallet_outlined,
-                              color: Colors.purpleAccent)),
-                      keyboardType: TextInputType.number,
-                    ),
-                  ),
-                ]),
-              ),
-              const Divider(),
-              Container(
-                padding: const EdgeInsets.only(top: 20),
-                child: Column(children: [
-                  Container(
-                    width: 220,
-                    height: 80,
-                    padding: const EdgeInsets.all(7),
-                    decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(50)),
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(color: Colors.black, blurRadius: 5)
-                        ]),
-                    child: TextFormField(
-                      controller: controllerMantencion,
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return "Ingresar costo de mantencion";
-                        }
-                      },
-                      decoration: const InputDecoration(
-                          hintText: "Costo Mantención",
-                          icon: Icon(Icons.account_balance_wallet_outlined,
-                              color: Colors.purpleAccent)),
-                      keyboardType: TextInputType.number,
-                    ),
-                  ),
-                ]),
-              ),
-              const Divider(
-                height: 20.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  TextButton(
-                    onPressed: OperacionMatematica,
-                    style: TextButton.styleFrom(
-                      primary: Colors.teal,
-                      shape: const BeveledRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(5))),
-                    ),
-                    child: Column(
-                      children: const <Widget>[
-                        Icon(Icons.person),
-                        Text('Calcular')
-                      ],
-                    ),
-                  ),
-                  const VerticalDivider(),
-                  TextButton(
-                    onPressed: OperacionMatematica,
-                    style: TextButton.styleFrom(
-                      primary: Colors.amber,
-                      shape: const BeveledRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(5))),
-                    ),
-                    child: Column(
-                      children: const <Widget>[
-                        Icon(Icons.person),
-                        Text('Calculamos')
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              const Divider(
-                height: 40.0,
-              ),
-              Container(
-                height: 150,
-                width: 300,
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                      colors: [Color(0xFFFE2E64), Color(0xFFfF7818)]),
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
+              TextButton(
+                onPressed: OperacionMatematica,
+                style: TextButton.styleFrom(
+                  primary: Colors.amber,
+                  shape: const BeveledRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(5))),
                 ),
-                //creamos el resultado del textformfield
-                child: Center(
-                  child: Text(
-                    mostrarImc,
-                    style: const TextStyle(fontSize: 20.0),
-                    textAlign: TextAlign.center,
-                  ),
+                child: Column(
+                  children: const <Widget>[
+                    Icon(Icons.calculate_outlined),
+                    Text('Calcular')
+                  ],
                 ),
               ),
-              Divider(),
-              Container(
-                height: 150,
-                width: 300,
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                      colors: [Color(0xFFFE2E64), Color(0xFFfF7818)]),
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
+              TextButton(
+                onPressed: limpiaCampos,
+                style: TextButton.styleFrom(
+                  primary: const Color.fromARGB(255, 52, 28, 236),
+                  shape: const BeveledRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(5))),
                 ),
-                //creamos el resultado del textformfield
-                child: Center(
-                  child: Text(
-                    mostrarNumOrdenes,
-                    style: const TextStyle(fontSize: 20.0),
-                    textAlign: TextAlign.center,
-                  ),
+                child: Column(
+                  children: const <Widget>[
+                    Icon(Icons.cleaning_services_outlined),
+                    Text('Limpiar')
+                  ],
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  primary: Color.fromARGB(255, 255, 1, 1),
+                  shape: const BeveledRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(5))),
+                ),
+                child: Column(
+                  children: const <Widget>[
+                    Icon(Icons.info_outline),
+                    Text('Formulas')
+                  ],
                 ),
               ),
             ],
           ),
         ),
-      ),
+        Container(
+          height: 100,
+          width: 30,
+          decoration: const BoxDecoration(
+            gradient:
+                LinearGradient(colors: [Color(0xFFFE2E64), Color(0xFFfF7818)]),
+          ),
+          //creamos el resultado del textformfield
+          child: Center(
+            child: Text(
+              mostrarImc,
+              style: const TextStyle(fontSize: 20.0),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+        ),
+        Container(
+          height: 100,
+          width: 30,
+          decoration: const BoxDecoration(
+            gradient:
+                LinearGradient(colors: [Color(0xFFFE2E64), Color(0xFFfF7818)]),
+          ),
+          //creamos el resultado del textformfield
+          child: Center(
+            child: Text(
+              mostrarNumOrdenes,
+              style: const TextStyle(fontSize: 20.0),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
--- lib/screens/vistalista.dart
+++ lib/screens/vistalista.dart
@@ -2,12 +2,6 @@
 import 'package:flutter/material.dart';
 
 class VistaListaScreen extends StatelessWidget {
-  final options = const [
-    'Calculadora EOQ Básica',
-    'Calculadora EOQ con faltante',
-    'Opciones'
-  ];
-
   const VistaListaScreen({Key? key}) : super(key: key);
 
   @override
@@ -15,9 +9,105 @@ class VistaListaScreen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: const Text('Calculadora con Faltante'),
+          elevation: 0,
+          backgroundColor: Colors.indigo,
         ),
-        body: ListView(children: const <Widget>[
-          Text('Aqui deberia ir una calculadora con faltante'),
-        ]));
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(" Demanda (D): "),
+                Container(
+                  width: 60,
+                  height: 50,
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+                const Text("Costo Orden (K)"),
+                Container(
+                  width: 60,
+                  height: 50,
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child:
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                const Text("Mantención (H): "),
+                Container(
+                  width: 60,
+                  height: 50,
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+                const Text("Costo unitario (i): "),
+                Container(
+                  width: 60,
+                  height: 50,
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                  ),
+                )
+              ]),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    primary: Colors.amber,
+                    shape: const BeveledRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(5))),
+                  ),
+                  child: Column(
+                    children: const <Widget>[
+                      Icon(Icons.calculate_outlined),
+                      Text('Calcular')
+                    ],
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    primary: const Color.fromARGB(255, 52, 28, 236),
+                    shape: const BeveledRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(5))),
+                  ),
+                  child: Column(
+                    children: const <Widget>[
+                      Icon(Icons.cleaning_services_outlined),
+                      Text('Limpiar')
+                    ],
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    primary: Color.fromARGB(255, 255, 1, 1),
+                    shape: const BeveledRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(5))),
+                  ),
+                  child: Column(
+                    children: const <Widget>[
+                      Icon(Icons.info_outline),
+                      Text('Formulas')
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ));
   }
 }
